648d7c0a4f7bb855b58375c83117276d8f63c8df
Ch4nYH <hfercxx@hotmail.com>
2020-05-03 20:45:12 +0800

    update

--- optimizers.py
+++ optimizers.py
@@ -93,9 +93,12 @@ class ChannelWiseOptimizer(object):
             state['vt_hat'] = state['vt'] / (1 - np.power(self.beta2, state['t']))
             
             for i in range(p.grad.shape[0]):
-                p.grad.data[i, ...].mul_(self.mask[name.split('.')[0] + "_" + str(i)])
-            
-            p.data.add_(self.alpha * self.state[p]['mt_hat'] / (torch.sqrt(self.state[p]['vt_hat']) + self.eta))
+                try:
+                    p.grad.data[i, ...].mul_(self.mask[name.split('.')[0] + "_" + str(i)])
+                except KeyError:
+                    print(name.split('.')[0] + "_" + str(i), "not found.")
+                p.data.add_(self.alpha * self.state[p]['mt_hat'] / (torch.sqrt(self.state[p]['vt_hat']) + self.eta))
+           
 
     def zero_grad(self):
         for p in self.parameters:
